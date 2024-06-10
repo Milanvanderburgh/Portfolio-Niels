214909edef8e03f2b18cb8d971e78f87517cf7d1
MiguelBrugge <103996305+MiguelBrugge@users.noreply.github.com>
2024-06-10 20:46:49 +0200

    added lazyloading

--- src/components/ProjectElement.jsx
+++ src/components/ProjectElement.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
+import LazyLoad from 'react-lazyload';
 
 const ProjectElement = ({ project, setCurrentPage }) => {
 
-  const { image, alt, title, description, labels, type } = project;  
+  const { image, alt, title, description, labels, type } = project;
 
   const handleClick = () => {
     setCurrentPage();
@@ -10,11 +11,11 @@ const ProjectElement = ({ project, setCurrentPage }) => {
 
   return (
     <div className="col-md-5 projectElement" onClick={handleClick}>
-      
-      <img src={image} alt={alt} height={250} width={300}/>
+      <LazyLoad height={250} offset={100}>
+        <img src={image} alt={alt} height={250} width={300} />
+      </LazyLoad>
       <div className="background d-flex flex-column p-3 pb-0 rounded">
         <div className="d-flex justify-content-between align-items-center"><h5 className='mainColor d-flex mb-3'>{title}</h5>{type ? <p className="label fw-bold end-0 p-2">{type}</p> : ""}</div>
-        
         <p>{description}</p>
         <p className='fst-italic mt-auto mb-0'>{labels}</p>
       </div>
